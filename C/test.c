1606972da4d3fbc249f306403f54efd296cca84b
heenamkim <znzltiq111@naver.com>
2020-02-07 21:21:53 +0900

    python

--- C/test.c
+++ C/test.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-  printf("Hello world\n");
+  int result1,result2;
+  int num1,num2;
+
+  scanf("%d %d",&num1,&num2 );
+  result1=num1/num2;
+  result2=num1%num2;
+  printf("%d/%d=%d\n",num1,num2,result1);
+  printf("%d %d=%d",num1,num2,result2);
   return 0;
 }
