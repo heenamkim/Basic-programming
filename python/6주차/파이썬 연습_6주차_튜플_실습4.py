31fb8ff209e34a775f1d363ec1bc52870dc95f6c
heenamkim <znzltiq111@naver.com>
2020-02-12 23:48:17 +0900

    python

--- python/6주차/파이썬 연습_6주차_튜플_실습4.py
+++ python/6주차/파이썬 연습_6주차_튜플_실습4.py
@@ -1,3 +1,24 @@
 #파이썬 연습_6주차_튜플_실습4.py
 
+s_num=int(input())
 
+total=[]
+
+def camltbeAdded(num):
+    for i in num:
+        if int(i) % s_num == 0 or s_num % int(i) == 0:
+            total.append(int(i))
+    return total
+
+
+while True:
+    print('1.Add\n2.Print\n0.exit')
+    m=int(input())
+    if m == 1:
+        num=input().split()
+        camltbeAdded(num)
+    elif m == 2:
+        print(tuple(camltbeAdded(num)))
+    else:
+        break
+        print('0')
