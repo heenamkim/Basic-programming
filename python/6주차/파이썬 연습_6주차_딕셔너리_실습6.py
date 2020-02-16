88769e163d26e38d52fccd5a15cfbb8d5aa46a3d
heenamkim <znzltiq111@naver.com>
2020-02-17 02:10:59 +0900

    python

--- python/6주차/파이썬 연습_6주차_딕셔너리_실습6.py
+++ python/6주차/파이썬 연습_6주차_딕셔너리_실습6.py
@@ -1,3 +1,15 @@
 #파이썬 연습_6주차_딕셔너리_실습6.py
 
-s_num=int(input())
+result={}
+while True:
+    name=input()
+    if name == 'None':
+        break
+    else:    
+        sub=input().split()
+        total=0
+        for i in sub:
+            total+=int(i)
+    result[name]=total/3
+           
+print(result)
--- python/6주차/파이썬 연습_6주차_딕셔너리_실습7.py
+++ python/6주차/파이썬 연습_6주차_딕셔너리_실습7.py
@@ -1,13 +1,21 @@
 #파이썬 연습_6주차_딕셔너리_실습6.py
 
 result={}
-total=0
+
 while True:
     name=input()
     if name == 'None':
         break
     else:    
         sub=input().split()
+        list1=[]
+        avg_list=[]
+        total=0
         for i in sub:
-            result[name]=(total+int(i))/3
+            list1.append(int(i))
+            result[name]=list1
+            i=list1[i]
+          
 print(result)
+
+
