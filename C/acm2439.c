0f373dd7f46a4823f46497b2187054043a664fd8
heenamkim <znzltiq111@naver.com>
2020-02-25 17:25:28 +0900

    C

---
+++ C/acm2439.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int N,i,j,k;
+  scanf("%d",&N);
+
+  for(i=0;i<N;i++){
+    for(k=0;k<N-i;k++){
+      printf(" ");
+    }
+      for(j=0;j<=i;j++){
+  	printf("*");
+      }
+      printf("\n");
+  }
+}
--- C/acm2588.c
+++ C/acm2588.c
@@ -2,11 +2,16 @@
 
 int main()
 {
-  int A,B;
-
-
+  int A,B,a,b,c,d;
   scanf("%d",&A);
   scanf("%d",&B);
-  printf("%d",A*B);
+  a=B%10;
+  b=B/10;
+  d=b%10;
+  c=B/100;
+  printf("%d\n",A*a);
+  printf("%d\n",A*d);
+  printf("%d\n",A*c);
+  printf("%d\n",A*B);
   return 0;
 }
