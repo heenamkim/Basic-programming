3b85ac1f7e9c36952fcc145b25d993d7bf93f98d
heenamkim <znzltiq111@naver.com>
2020-02-27 23:30:12 +0900

    python

--- python/6주차/파이썬 연습_6주차_딕셔너리_실습7.py
+++ python/6주차/파이썬 연습_6주차_딕셔너리_실습7.py
@@ -1,7 +1,6 @@
 #파이썬 연습_6주차_딕셔너리_실습7.py
 
 result={}
-
 while True:
     name=input()
     if name == 'None':
@@ -9,14 +8,9 @@ while True:
     else:    
         sub=input().split()
         list1=[]
-        python=0
-        total=0
         for i in sub:
             list1.append(int(i))
             result[name]=list1
-        python=list1[0]
-        total=sum(python)/3
-print(total)
 print(result)
 
 
