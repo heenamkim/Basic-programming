e1f732ad679c5d75eefc5e89c456e2410eea247a
heenamkim <znzltiq111@naver.com>
2020-03-01 23:13:23 +0900

    python

--- python/6주차/파이썬 연습_6주차_딕셔너리_실습8.py
+++ python/6주차/파이썬 연습_6주차_딕셔너리_실습8.py
@@ -1,30 +1,33 @@
 #파이썬 연습_6주차_딕셔너리_실습8.py
 
 dic={'admin':'root'}
+count=0
 while True:
     print('1.Signed up\n2.Sianen in\n0.exit')
-    num=int(input())
+    num=input()
     if num == 0:
         print(dic)
         break
     elif num == 1:
         new_id=input('ID =')
-        new_pw=int(input('PW ='))
+        new_pw=input('PW =')
         dic[new_id]=new_pw
         print('Joined Complete!')
     else:
         l_id=input('ID =')
-        I_pw=int(input('PW ='))
-        for i in 5 :
-            if dic[i] == i:
-                print('Loged in Complete!')
-            elif dic[i] != i:
-                print("Wrong rassword! Try", i,'/5')
-                l_pw=int(input('PW ='))
-                if count > 5 :
-                    print("There's no account. Please join us!")
-                    print('password')
-                    new_pw=int(input('New PW ='))
-                    
-
+        for key, value in dic.items():
+            if key in dic != None:
+                print("There's no account. Please join us!")
+            else:
+                I_pw=input('PW =')
+                if key and value !=None:
+                    print('Loged in Complete!')
+                else:
+                    for i in range(5):
+                        if count<5:
+                            count+=1
+                            print("Wrong rassword! Try", count,'/5')
+                        else:
+                            I_pw=input('PW =')
+            
 
--- python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
+++ python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
@@ -1,18 +1,6 @@
 
 
-total=[2, 5, 4, 24, 3, 6, 64, 5, 3]
-python=0
-c=0
-java=0
-for i in range(len(total)):
-    if i%3==1:
-        python+=total[i]
-    elif i%3==2:
-        c+=total[i]
-    elif i%3==0:
-        java+=total[i]
-        
-print('python',python)
-print('c',c)   
-print('java',java)        
-print(total)
+dic={'kim':123,'lee':234,'park':233}
+for key,value in dic.items():
+    if key == value:
+
