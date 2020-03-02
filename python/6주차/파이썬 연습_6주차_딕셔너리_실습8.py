c476520f0a1c6d65edca686b243aa30ef49844ef
heenamkim <znzltiq111@naver.com>
2020-03-02 15:01:01 +0900

    python

--- python/6주차/파이썬 연습_6주차_딕셔너리_실습8.py
+++ python/6주차/파이썬 연습_6주차_딕셔너리_실습8.py
@@ -4,7 +4,7 @@ dic={'admin':'root'}
 count=0
 while True:
     print('1.Signed up\n2.Sianen in\n0.exit')
-    num=input()
+    num=int(input())
     if num == 0:
         print(dic)
         break
@@ -14,13 +14,12 @@ while True:
         dic[new_id]=new_pw
         print('Joined Complete!')
     else:
-        l_id=input('ID =')
-        for key, value in dic.items():
-            if key in dic != None:
+        new_id=input('ID =')
+        for new_id in dic:
                 print("There's no account. Please join us!")
             else:
-                I_pw=input('PW =')
-                if key and value !=None:
+                new_pw=input('PW =')
+                if new_id == dic[new_id]:
                     print('Loged in Complete!')
                 else:
                     for i in range(5):
@@ -28,6 +27,8 @@ while True:
                             count+=1
                             print("Wrong rassword! Try", count,'/5')
                         else:
-                            I_pw=input('PW =')
-            
+                            new_pw=input('PW =')
+                            dic[new_id]=l_pw
+        
+
 
