3d8ff58ec22c47c7dc85ab656526959345d2586a
heenamkim <znzltiq111@naver.com>
2020-03-09 16:19:18 +0900

    python

--- python/5주차/파이썬 연습_5주차_리스트_추가 공부자료_실습7.py
+++ python/5주차/파이썬 연습_5주차_리스트_추가 공부자료_실습7.py
@@ -2,14 +2,10 @@
 
 num=input().split() #num를 list로 여러게 한번에 받기
 list1=[]
+Max = int(num[0])
 for i in range(len(num)):
     for i in num :
-        if i not in list1:
-            Max = int(num[0])
-            if Max < int(i):
-                Max = int(i)
-        list1.append(Max)
-
+        if Max < int(i) and i not in list1:
+            Max = int(i)
+    list1.append(Max)
 print(list1)
-
-    
