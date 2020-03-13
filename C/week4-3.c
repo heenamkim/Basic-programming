34aef6a5664525401311e13bc5699b478dee612b
heenamkim <znzltiq111@naver.com>
2020-03-13 22:54:15 +0900

    python

--- C/week4-3.c
+++ C/week4-3.c
@@ -5,7 +5,7 @@ int main()
 int n;
 int count=0;
 scanf("%d",&n);
-for (int i=0; i<=n;i++)
+for (int i=1; i<=n;i++)
   if (n%i==0)
     count++;
 printf("Count = %d",count);
--- python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
+++ python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
@@ -2,24 +2,23 @@
 
 import random
 card = [1,2,3,4,5,6,7,8,9,10,'J','Q','K']
-random.shuffle(card)
 people=['You','Player']
-first=random.shuffle(people)
 a=0
 b=0
-print(people[0],'First!')
-people[0]=card[:3]
-people[1]=card[3:6]
-if people[0] == 'You':
-    print('You card is1',people[0])
-else:
-    print('you card is2',people[1])
+you=[]
+player=[]
 for i in range(3):
-    you=input('Your turn >>')
-    print('Player >>',player[i])
-    if int(you) > int(player[i]):
-        a+=1
-        print("==score [",a,":",b,"]==")            
+    if i < 3:
+        d=random.choice(card)    
+        you.append(d)
+        card.remove(d)
+        player.append(d)
+        card.remove(d) ''' 
     else:
-        b+=1
-        print("==score [",a,":",b,"]==")
+        d=random.choice(card)
+        player.append(d)
+        card.remove(d)
+        '''
+        
+print(you)
+print(player)
