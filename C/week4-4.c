1c4d695aee34a1550fdb735c908788c8dea2d12a
heenamkim <znzltiq111@naver.com>
2020-03-14 13:48:10 +0900

    python

--- C/week4-4.c
+++ C/week4-4.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-int n,a;
+char n;
+int totla;
 scanf("%d",&n);
-for (int i=0; i<=(n);i++)
-  a=n%10^i;
+for (int i=0; i<=n;i++)
+  total+=int(i)
 printf("%d",a);
 
 return 0;
--- python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
+++ python/염ㅅ브염스빙러ㅏㄴㅁㄹ.py
@@ -2,23 +2,59 @@
 
 import random
 card = [1,2,3,4,5,6,7,8,9,10,'J','Q','K']
+card[10]=11
+card[11]=12
+card[12]=13
 people=['You','Player']
 a=0
 b=0
 you=[]
 player=[]
-for i in range(3):
-    if i < 3:
-        d=random.choice(card)    
-        you.append(d)
-        card.remove(d)
-        player.append(d)
-        card.remove(d) ''' 
-    else:
-        d=random.choice(card)
-        player.append(d)
-        card.remove(d)
-        '''
-        
-print(you)
-print(player)
+first=random.choice(people)
+if first == 'You':
+    print('You First!')
+    for i in range(6):
+        if i < 3:
+            d=random.choice(card)    
+            you.append(d)
+            card.remove(d)
+        else:
+            d=random.choice(card)
+            player.append(d)
+            card.remove(d)
+    print('You card is',you)
+    for i in range(3):
+            y=input('Your turn >>')
+            p=random.choice(player)
+            player.remove(p)
+            print('Player >>',p)
+            if y > p:
+                a+=1
+                print("==score [",a,":",b,"]==")            
+            else:
+                b+=1
+                print("==score [",a,":",b,"]==")
+else:
+    print('Player First!')
+    for i in range(6):
+        if i < 3:
+            d=random.choice(card)    
+            player.append(d)
+            card.remove(d)
+        else:
+            d=random.choice(card)
+            you.append(d)
+            card.remove(d)            
+    print('You card is',you)
+    for i in range(3):
+            p=random.choice(player)
+            player.remove(p)
+            print('Player >>',p)
+            y=input('Your turn >>')
+            if y < p:
+                b+=1
+                print("==score [",a,":",b,"]==")            
+            else:
+                a+=1
+                print("==score [",a,":",b,"]==")
+
