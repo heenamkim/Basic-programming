430de159c93d44afb25078988e1e0d357921e604
heenamkim <znzltiq111@naver.com>
2020-03-18 15:45:51 +0900

    C

--- C/acm3052.c
+++ C/acm3052.c
@@ -5,10 +5,12 @@ int main()
 int num[10];
 int rem[10];
 int count=0;
-for (int i=0;i<10;i++)
+for (int i=0;i<10;i++){
   scanf("%d",&num[i]);
-  for (int j=0;j<10;j++)
-    rem[j]=(num[j]%42);
-for (int k=0;k<10;k++)
-  printf("%d\n",rem[k]);
+  rem[i]=num[i]%42;
+  for (int j=0;j<42;j++)
+    if (rem[10]==j)
+      count++;
+        }
+printf("%d\n",count);
 }
