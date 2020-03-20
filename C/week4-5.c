79d3c3ff3b7dbbe2a547691a428aa680884a0a2f
heenamkim <znzltiq111@naver.com>
2020-03-20 17:57:51 +0900

    C

--- C/week4-5.c
+++ C/week4-5.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <math.h> //오차법위 주의할것! round사용하기!
+// #include <cmath> c++할때 사용 오차법위 없음!!
 
+int main()
+{
+    int n, k=0, count, j=0;
+    int total=0,num=0, tmp;
+    scanf("%d",&n);
+    count = n;
+    while (count != 0){
+        count=count/10;
+        j++;
+     }
 
-int main() {
-char num[50];
-int total=0;
-scanf("%s", &num);
-for (int i = 0; num[i] != '\0'; i++){
-  total = num[i] - '0';
-  printf("%d\n", total);
+    for (int i=j-1; i>=0; i--) {
+
+        tmp = (int)round(pow(10, i));
+        num = n / tmp;
+        total += num*round(pow(10, k++));
+        n = n % tmp;
+    }
+     printf("%d\n",total);
 }
-return 0;
- }
--- python/5주차/파이썬 연습_5주차_리스트_실습6(카드게임).py
+++ python/5주차/파이썬 연습_5주차_리스트_실습6(카드게임).py
@@ -10,79 +10,51 @@ player=[]
 a=0
 b=0
 first=random.choice(people)
+
 if first == 'You':
     print('You First!')
     you=card[:3]
     player=card[3:6]
-    print('You card is',you)
-    print('Player card is',player)
-    for i in range(3):
-        you=input('Your turn >>')
-        p=random.choice(player)
-        player.remove(p)
-        print('Player >>',p)
-        if you in dic_card or p in dic_card:
-            if you > p:
-                a+=1
-                print("==score [",a,":",b,"]==")
-                if a==2:
-                    print("You Win!")
-                    break
-            else:
-                b+=1
-                print("==score [",a,":",b,"]==")
-                if b==2:
-                    print("You Lose~")
-                    break
-        else:
-            if int(you) > p:
-                a+=1
-                print("==score [",a,":",b,"]==")
-                if a==2:
-                    print("You Win!")
-                    break
-            else:
-                b+=1
-                print("==score [",a,":",b,"]==")
-                if b==2:
-                    print("You Lose~")
-                    break
-
-            
 else:
     print('Player First!')
     player=card[:3]
     you=card[3:6]
-    print('Player card is',player)
-    print('You card is',you)
-    for i in range(3):
+    
+print('You card is',you)
+print('Player card is',player)
+
+for i in range(3):
+    if first == 'You':
+        you=input('Your turn >>')
+        p=random.choice(player)
+        player.remove(p)
+        print('Player >>',p)
+    else:
         p=random.choice(player)
         player.remove(p)
         print('Player >>',p)
         you=input('Your turn >>')
-        if you in dic_card or p in dic_card:
-            if you < p:
-                a+=1
-                print("==score [",a,":",b,"]==")
-                if a==2:
-                    print("You Lose~")
-                    break
-            else:
-                b+=1
-                print("==score [",a,":",b,"]==")
-                if b==2:
-                    print("You Win!")
-                    break
-        else:
-            if int(you) < p:
-                a+=1
-                print("==score [",a,":",b,"]==")
-                if a==2:
-                    print("You Lose~")
-                    break
-            else:
-                b+=1
-                print("==score [",a,":",b,"]==")
-                if b==2:
-                    print("You Win!")
-                    break 
+        
+    if you in dic_card:
+        you = dic_card[you]
+    else:
+        you = int(you)
+
+    if p in dic_card:
+        p = dic_card[p]
+    else:
+        p = int(p)
+
+    if you > p:
+        a+=1
+        print("==score [",a,":",b,"]==")
+        if a==2:
+            print("You Win!")
+            break
+    else:
+        b+=1
+        print("==score [",a,":",b,"]==")
+        if b==2:
+            print("You Lose~")
+            break
+
