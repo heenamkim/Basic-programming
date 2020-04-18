42f1e414d9adc5239d330058fadd824786d67a7d
heenamkim <znzltiq111@naver.com>
2020-04-18 18:05:01 +0900

    baekjoon

--- C/Baekjoon/acm2386.c
+++ C/Baekjoon/acm2386.c
@@ -5,14 +5,15 @@
 int main()
 {
     char sentence[250]={0};
-    char alp, len;
+    char alp;
+    int len;
 
     while(1){
         int count = 0;
-        scanf("%s", &alp);
+        scanf("%c", &alp);
         if (alp == '#')
             break;
-        scanf("%[^\n]s", &sentence);
+        scanf("%[^\n]s", sentence); //수정
         len = strlen(sentence);
         for (int i = 0; i < len; i++){
             if (alp == sentence[i] || alp == sentence[i]+32)
