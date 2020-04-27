63d2e9ee12ec078b1938dbe4693a9739b5c772e5
heenamkim <znzltiq111@naver.com>
2020-04-27 23:36:46 +0900

    week5

---
+++ C/Advanced/Section5-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char N;
+
+    while(scanf("%c", &N) != EOF){
+
+        for (int i = 'A'; i < 'Z'; i++){
+            if (i == N){
+                printf("Upper case\n");
+            }
+        }
+        for (int i = 'a'; i < 'z'; i++){
+            if (i == N){
+                printf("Lower case\n");
+            }
+        }
+        for (int i = '0'; i < '9'; i++){
+            if (i == N){
+                printf("Arabic number\n");
+            }
+        }
+    }
+    printf("END");
+}
