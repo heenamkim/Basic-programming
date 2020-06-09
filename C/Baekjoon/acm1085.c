41cf04547b59f1c624c506fe2368ab9ed346780c
heenamkim <znzltiq111@naver.com>
2020-06-09 23:13:13 +0900

    backjoon

--- C/Baekjoon/acm1085.c
+++ C/Baekjoon/acm1085.c
@@ -3,10 +3,22 @@
 int main()
 {
 
-    int arr[5];
+    int arr[4];
     int x,y,w,h;
+    int min;
 
     scanf("%d %d %d %d", &x, &y, &w, &h);
 
+    min = x;
+    arr[0] = x;
+    arr[1] = y;
+    arr[2] = w-x;
+    arr[3] = h-y;
 
+    for (int i = 0; i < 5; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    printf("%d", min);
 }
