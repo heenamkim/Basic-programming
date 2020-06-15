43c721b014b3f9d1d2dfd1f17b784071f58fe07f
heenamkim <znzltiq111@naver.com>
2020-06-15 23:03:52 +0900

    baekjoon

--- C/Baekjoon/acm1712.c
+++ C/Baekjoon/acm1712.c
@@ -3,20 +3,13 @@
 int main()
 {
     int a, b, c;
-    int ab_total = 0, c_total = 0;
-    int amount = 1;
 
     scanf("%d %d %d", &a, &b, &c);
 
-    while(1) {
-        ab_total = a + (b * amount);
-        c_total = c * amount;
-        if (ab_total < c_total) {
-            printf("%d", amount);
-            break;
-        }
-        else (
-            amount++;
-        )
+    if (c <= b) {
+        printf("-1");
+    }
+    else {
+        printf("%d", a/(c-b)+1);
     }
 }
