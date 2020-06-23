72ab49dd5f8c3fadf336c73484f3315981297ec8
heenamkim <znzltiq111@naver.com>
2020-06-23 23:49:12 +0900

    baekjoon

--- C/Baekjoon/acm4673.c
+++ C/Baekjoon/acm4673.c
@@ -7,17 +7,6 @@ int main()
     int number = 0;
 
     for (int i = 1; i <= 10; i++) {
-        total++;
-        a = i % 10;
-        b = i / 10;
-        for (int j = 1; j <= i; j++) {
-            if (j != i+a+b) {
-                number = 1;
-                total = j;
-            }
-        }
-        if (number == 1) {
-            printf("%d\n", total);
-        }
+    
     }
 }
