bf88ce6470757447aa28785c23867dd89ca08980
heenamkim <znzltiq111@naver.com>
2020-06-30 23:56:46 +0900

    section6

--- C/Advanced/Section6-2.c
+++ C/Advanced/Section6-2.c
@@ -1,20 +1,32 @@
 #include<stdio.h>
 
-int DicInt(int *num1, int *num2)
-{
-    int portion, rest;
+int DicInt(int *a, int *b);
 
-    portion = *num1 / *num2;
-    rest = *num1 % *num2;
-
-    return rest;
-}
 int main()
 {
-    int num1, num2;
+    int quo = 0, rem = 0, fail;
+
+    fail = DicInt(&quo, &rem);
+    if (fail == 1) {
+        printf("몫: %d, 나머지: %d", quo, rem);
+    }
+    else {
+        printf("오류 발생!!");
+    }
+}
+
+int DicInt(int *a, int *b)
+{
+    int num1 = 0, num2 = 0, result = 0;;
 
     scanf("%d %d", &num1, &num2);
-
-
-    printf("몫: %d", rest);
+    if (num2 == 0) {
+        result = -256;
+    }
+    else {
+        *a = num1 / num2;
+        *b = num1 % num2;
+        result = 1;
+    }
+    return result;
 }
